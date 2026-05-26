ccbde99c05e43fed77b7ab8b5ccad2ab67b6e1fc
Abhishek Meena <abhixkakroda@gmail.com>
2026-05-27 01:07:21 +0530

    Remove View All from experiences

--- src/features/portfolio/components/experiences/index.tsx
+++ src/features/portfolio/components/experiences/index.tsx
@@ -1,8 +1,4 @@
-import { ArrowUpRightIcon } from "lucide-react"
-import Link from "next/link"
 import React from "react"
-
-import { Button } from "@/components/base/ui/button"
 
 import { EXPERIENCES } from "../../data/experiences"
 import { Panel, PanelHeader, PanelTitle } from "../panel"
@@ -23,18 +19,6 @@ export function Experiences() {
         {workExperiences.map((experience) => (
           <ExperienceItem key={experience.id} experience={experience} />
         ))}
-      </div>
-
-      <div className="screen-line-top flex justify-center py-2">
-        <Button
-          className="gap-2 border-none pr-2.5 pl-3"
-          size="sm"
-          nativeButton={false}
-          render={<Link href="/experience.md" />}
-        >
-          View All
-          <ArrowUpRightIcon />
-        </Button>
       </div>
     </Panel>
   )
